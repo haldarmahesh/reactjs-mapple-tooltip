3e1d569b789e60e7655ebecbfd02d79735dd6c8d
Mahesh Haldar <haldar.mahesh@gmail.com>
2016-07-24 12:27:00 +0530

    Remove dead code

--- js/components/ToolTip/index.js
+++ js/components/ToolTip/index.js
@@ -45,6 +45,14 @@ export default class ToolTip extends Component {
       </span>
     );
   }
+  getPlateAndMappleInfo() {
+    const plateDom = new Dom(this.refs.plateComp).getDomInfo();
+    const contentForMapple = new Dom(this.refs.contentForMapple).getDomInfo();
+    return {
+      plate: plateDom,
+      mapple: contentForMapple
+    }
+  }
   setPropsValues(props) {
     return {
       mouseIsOver: false,
@@ -61,45 +69,15 @@ export default class ToolTip extends Component {
       default: true
     };
   }
-  getPositionAroundCursor(referenceXY) {
-    const plateDom = new Dom(this.refs.plateComp).getDomInfo();
-    const contentForMapple = new Dom(this.refs.contentForMapple).getDomInfo();
-    let distanceX = 0;
-    let distanceY = 0;
-    let posX = 0;
-    let posY = 0;
-    if (this.state.direction === 'top') {
-      distanceX = 0;
-      distanceY = -this.gap;
-      posX = referenceXY.x - contentForMapple.left - plateDom.width / 2 + distanceX;
-      posY = referenceXY.y - contentForMapple.top - plateDom.height + distanceY;
-    } else if (this.state.direction === 'right') {
-      distanceX = this.gap + 5;
-      distanceY = 0;
-      posX = referenceXY.x - contentForMapple.left + distanceX;
-      posY = referenceXY.y - plateDom.height/2 - contentForMapple.top;
-    } else if (this.state.direction === 'bottom') {
-      distanceX = 0;
-      distanceY = this.gap + 10;
-      posX = referenceXY.x - contentForMapple.left - plateDom.width / 2 + distanceX;
-      posY = referenceXY.y - contentForMapple.top + distanceY;
-      
-    } else if (this.state.direction === 'left') {
-      
-    }
-    return {
-      x: posX,
-      y: posY
-    }
-  }
   handleMouseEnter(event) {
     const position = new Position();
     const fixedXY = position.getFixedCoordinates(event);
     const widthHeight = position.getWidthHeight(event);
     const plateDom = new Dom(this.refs.plateComp);
     const plateDimensions = plateDom.getDimensions();
-    const contentForMapple = new Dom(this.refs.contentForMapple).getDomInfo();
-    const newPositionAroundDom = position.getPositionAroundDom(this.state.direction, plateDom.getDomInfo(), contentForMapple);
+    const plateDomInfo = this.getPlateAndMappleInfo().plate;
+    const contentForMapple = this.getPlateAndMappleInfo().mapple;
+    const newPositionAroundDom = position.getPositionAroundDom(this.state.direction, plateDomInfo, contentForMapple);
     const newPositionAroundCursor = position.getPositionAroundCursor(fixedXY, this.state.direction, plateDom.getDomInfo(), contentForMapple);
     this.setState({
       plateWidthHeight: plateDimensions
@@ -128,9 +106,9 @@ export default class ToolTip extends Component {
     }
     const position = new Position(event);
     const mousePosition = position.getFloatCoordinates(event);
-    const plateDom = new Dom(this.refs.plateComp);
-    const contentForMapple = new Dom(this.refs.contentForMapple).getDomInfo();
-    const newPositionAroundCursor = position.getPositionAroundCursor(mousePosition, this.state.direction, plateDom.getDomInfo(), contentForMapple);
+    const plateDom = this.getPlateAndMappleInfo().plate;
+    const contentForMapple = this.getPlateAndMappleInfo().mapple;
+    const newPositionAroundCursor = position.getPositionAroundCursor(mousePosition, this.state.direction, plateDom, contentForMapple);
     
     this.checkIfPlateGoingOut();
     this.setState({
